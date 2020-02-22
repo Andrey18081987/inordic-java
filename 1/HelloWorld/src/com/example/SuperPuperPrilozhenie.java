9ea43203e1728b04c9e2f6efa0a36338d9e80f19
George Great <gosha8352@gmail.com>
2020-02-22 12:23:39 +0300

    Update SuperPuperPrilozhenie.java
    Я вернул свой код

--- 1/HelloWorld/src/com/example/SuperPuperPrilozhenie.java
+++ 1/HelloWorld/src/com/example/SuperPuperPrilozhenie.java
@@ -7,7 +7,7 @@ public class SuperPuperPrilozhenie {
 	public static void main(String[] args) {
 		var input = new Scanner(System.in);
 		
-		System.out.print("Сколько мне лет?");
+		System.out.print("Сколько тебе лет?");
 		
 		var age = input.nextInt();
 		
